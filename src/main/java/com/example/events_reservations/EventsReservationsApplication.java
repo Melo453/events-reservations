001755ae2a7a18a9eddd447f145f5870913197e5
Fabrizio Meloni <fabrimeloni1@gmail.com>
2025-03-01 23:20:43 -0300

    creando proyecto

---
+++ src/main/java/com/example/events_reservations/EventsReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.events_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsReservationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/events_reservations/EventsReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.events_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
